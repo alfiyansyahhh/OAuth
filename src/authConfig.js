2ec9535d14c87fda1aab0384107c00945edf9f6a
m ilham alfiyansyah <muhamadilhamalfiyansyah@gmail.com>
2024-09-10 11:25:46 +0700

    add env

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,10 +1,20 @@
+const isLocal = process.env.REACT_APP_ENV === 'local';
+
 const authConfig = {
-  authority: process.env.REACT_APP_AUTHORITY,
-  client_id: process.env.REACT_APP_CLIENT_ID,
-  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  authority: isLocal
+    ? process.env.REACT_APP_AUTHORITY
+    : process.env.REACT_APP_AUTHORITY,
+  client_id: isLocal
+    ? process.env.REACT_APP_CLIENT_ID
+    : process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: isLocal
+    ? process.env.REACT_APP_REDIRECT_URI_LOCAL
+    : process.env.REACT_APP_REDIRECT_URI,
   response_type: process.env.REACT_APP_RESPONSE_TYPE,
   scope: process.env.REACT_APP_SCOPE,
-  post_logout_redirect_uri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
+  post_logout_redirect_uri: isLocal
+    ? process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI_LOCAL
+    : process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
   userinfo_endpoint: process.env.REACT_APP_USERINFO_ENDPOINT,
   response_mode: process.env.REACT_APP_RESPONSE_MODE,
   code_challenge_method: process.env.REACT_APP_CODE_CHALLENGE_METHOD,
