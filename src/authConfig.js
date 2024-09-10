1f384ae964ec11af8e09c5e9e9000b741723fead
m ilham alfiyansyah <muhamadilhamalfiyansyah@gmail.com>
2024-09-10 11:36:14 +0700

    .

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,20 +1,13 @@
-const isLocal = process.env.REACT_APP_ENV === 'local';
-
 const authConfig = {
-  authority: isLocal
-    ? process.env.REACT_APP_AUTHORITY
-    : process.env.REACT_APP_AUTHORITY,
-  client_id: isLocal
-    ? process.env.REACT_APP_CLIENT_ID
-    : process.env.REACT_APP_CLIENT_ID,
-  redirect_uri: isLocal
-    ? process.env.REACT_APP_REDIRECT_URI_LOCAL
-    : process.env.REACT_APP_REDIRECT_URI,
+  authority: process.env.REACT_APP_AUTHORITY,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  redirect_uri_local: process.env.REACT_APP_REDIRECT_URI_LOCAL,
   response_type: process.env.REACT_APP_RESPONSE_TYPE,
   scope: process.env.REACT_APP_SCOPE,
-  post_logout_redirect_uri: isLocal
-    ? process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI_LOCAL
-    : process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
+  post_logout_redirect_uri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
+  post_logout_redirect_uri_local:
+    process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI_LOCAL,
   userinfo_endpoint: process.env.REACT_APP_USERINFO_ENDPOINT,
   response_mode: process.env.REACT_APP_RESPONSE_MODE,
   code_challenge_method: process.env.REACT_APP_CODE_CHALLENGE_METHOD,
