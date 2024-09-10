5893a7625610ea4008b5d4e3acee423f414c8c15
m ilham alfiyansyah <muhamadilhamalfiyansyah@gmail.com>
2024-09-10 11:12:51 +0700

    test oidc

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,13 +1,13 @@
 const authConfig = {
-    authority: 'https://some_text.zitadel.cloud/', //Replace with your issuer URL
-    client_id: 'ABC123@Project', //Replace with your client id
-    redirect_uri: 'http://localhost:3000/callback',
-    response_type: 'code',
-    scope: 'openid profile email',
-    post_logout_redirect_uri: 'http://localhost:3000/',
-    userinfo_endpoint: 'https://instance-some_text.zitadel.cloud/oidc/v1/userinfo', //Replace with your user-info endpoint
-    response_mode: 'query',
-    code_challenge_method: 'S256',
-  };
+  authority: process.env.REACT_APP_AUTHORITY,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  response_type: process.env.REACT_APP_RESPONSE_TYPE,
+  scope: process.env.REACT_APP_SCOPE,
+  post_logout_redirect_uri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI,
+  userinfo_endpoint: process.env.REACT_APP_USERINFO_ENDPOINT,
+  response_mode: process.env.REACT_APP_RESPONSE_MODE,
+  code_challenge_method: process.env.REACT_APP_CODE_CHALLENGE_METHOD,
+};
 
- export default authConfig;
+export default authConfig;
